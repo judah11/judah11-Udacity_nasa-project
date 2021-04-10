ce74b01f0f2b127ff099695a0b373e90a927cd93
Judah L <joelio65@protonmail.com>
2021-04-09 21:38:06 -0700

    Updating example calls to rover endpoint

--- src/server/index.js
+++ src/server/index.js
@@ -3,9 +3,12 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const fetch = require('node-fetch')
 const path = require('path')
+const Immutable = require('immutable');
+
 
 const app = express()
 const port = 3000
+const rover_endpoint = 'https://api.nasa.gov/mars-photos/api/v1/';
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -15,6 +18,7 @@ app.use('/', express.static(path.join(__dirname, '../public')))
 // your API calls
 
 // example API call
+/*
 app.get('/apod', async (req, res) => {
     try {
         let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
@@ -24,5 +28,14 @@ app.get('/apod', async (req, res) => {
         console.log('error:', err);
     }
 })
-
+*/
+app.get('/apod', async (req, res) => {
+    try {
+        let image = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/latest_photos?api_key=${process.env.API_KEY}`)
+            .then(res => res.json())
+        res.send({ image })
+    } catch (err) {
+        console.log('error:', err);
+    }
+})
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
